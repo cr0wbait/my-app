ca4f53607a64fd1498bf10d84124b112337794db
iambateman <stephen.bateman@truematter.com>
2017-06-28 10:42:43 -0400

    fixing in memory api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
-import { HeroSearchComponent }  from './hero-search.component';
+// import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
   imports: [
@@ -28,8 +28,7 @@ import { HeroSearchComponent }  from './hero-search.component';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent,
-    HeroSearchComponent
+    HeroesComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
